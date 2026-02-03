cef22996d72b92046b1115d66e8a27144dc61080
votynovopages <tomas.vojtesek@gmail.com>
2026-02-03 18:45:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Jsi si fakt jistá?",
+"Opravdu opravdu??",
+"Jsi si úplně jistá?",
+"Lásko, prosím...",
+"Jen se nad tím zamysli!",
+"Když řekneš ne, budu smutný...",
+"Budu fakt smutný...",
+"Budu strašně, strašně smutný...",
+"Dobře, už přestanu s tím vyptáváním..",
+"Jen žertuji, prosím řekni ano! ❤️",
 ];
 
 let messageIndex = 0;
